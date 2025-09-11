81e0f1a21ba5e0a4c4a3ff080e15ebfacd4467a8
iran <iranilsonhonorato88@gmail.com>
2025-09-11 12:53:45 -0300

    feat(algorithm): implementação de um algoritmo que remove repetições

--- Algoritmos/src/main/java/NoRepetition/NoRepetition.java
+++ Algoritmos/src/main/java/NoRepetition/NoRepetition.java
@@ -3,5 +3,22 @@ package NoRepetition;
 import java.lang.reflect.Array;
 
 public class NoRepetition {
+    public int[] NoRepetitionFunction(int[] A,  int[] AClear) {
+        int index = 0;
+        for (int i = 0; i < A.length; i++) {
+            boolean found = false;
+            for (int j = 0; j < AClear.length; j++) {
+                if (A[i] == AClear[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                AClear[index] = A[i];
+                index ++;
+            }
 
+        }
+        return AClear;
+    }
 }
--- Algoritmos/src/main/java/org/example/Main.java
+++ Algoritmos/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import BubbleSort.BubbleSort;
+import NoRepetition.NoRepetition;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,7 +12,14 @@ public class Main {
     public static void main(String[] args) {
         BubbleSort bs = new BubbleSort();
         int[] A = {3, 5, 6, 8, 10, 1, 2, 7, 9, 4};
-        int[] sortedA = bs.BubbleSortFunction(A);
-        System.out.println(Arrays.toString(sortedA));
+        int[] resultbs = bs.BubbleSortFunction(A);
+        System.out.println(Arrays.toString(resultbs));
+
+
+        NoRepetition nr = new NoRepetition();
+        int[] B = {1,1,2,3,1,2,3,4};
+        int[] BClear = new int[4];
+        int[] resultNR = nr.NoRepetitionFunction(B, BClear);
+        System.out.println(Arrays.toString(resultNR));
     }
 }
