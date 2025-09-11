4993a543886254d4d8d489a888b29b29e820adc5
iran <iranilsonhonorato88@gmail.com>
2025-09-11 17:08:29 -0300

    feat(algorithm): Implementação do algoritmo de Selection Sort

---
+++ Algoritmos/src/main/java/SelectionSort/SelectionSort.java
@@ -0,0 +1,20 @@
+package SelectionSort;
+
+import java.util.ArrayList;
+
+public class SelectionSort {
+    public int[] SelectionSortFunction(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            int pivot = i;
+            for (int j = pivot + 1; j < A.length - 1; j++) {
+                if (A[pivot] > A[j]) {
+                    int aux = A[pivot];
+                    A[pivot] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+        return A;
+
+    }
+}
--- Algoritmos/src/main/java/org/example/Main.java
+++ Algoritmos/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import BubbleSort.BubbleSort;
 import NoRepetition.NoRepetition;
+import SelectionSort.SelectionSort;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,13 +14,18 @@ public class Main {
         BubbleSort bs = new BubbleSort();
         int[] A = {3, 5, 6, 8, 10, 1, 2, 7, 9, 4};
         int[] resultbs = bs.BubbleSortFunction(A);
-        System.out.println(Arrays.toString(resultbs));
+        System.out.println("Bubble Sort Algorithm: " + Arrays.toString(resultbs));
+
+
+        SelectionSort  ss = new SelectionSort();
+        int[] resultss = ss.SelectionSortFunction(A);
+        System.out.println("Selection Sort Algorithm: " + Arrays.toString(resultss));
 
 
         NoRepetition nr = new NoRepetition();
         int[] B = {1,1,2,3,1,2,3,4};
         int[] BClear = new int[4];
         int[] resultNR = nr.NoRepetitionFunction(B, BClear);
-        System.out.println(Arrays.toString(resultNR));
+        System.out.println("No Repetition on array: " + Arrays.toString(resultNR));
     }
 }
