c2c076d303da4c0a44012585b969d2fe339cb208
iran <iranilsonhonorato88@gmail.com>
2025-09-23 15:07:22 -0300

    feat(bubble-sort): Implementando algoritmo de bubble sort

--- src/main/java/algoritmos/Algoritmos.java
+++ src/main/java/algoritmos/Algoritmos.java
@@ -30,6 +30,20 @@ public class Algoritmos {
     };
 
 
-
+    public int[] bubbleSort(int[] A) {
+        boolean trocar = true;
+        while(trocar) {
+            trocar = false;
+            for(int i = 0; i <A.length-1; i++) {
+                if(A[i] > A[i+1]) {
+                    trocar = true;
+                    int aux = A[i];
+                    A[i] = A[i+1];
+                    A[i+1] = aux;
+                }
+            }
+        }
+        return A;
+    }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import algoritmos.Algoritmos;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +11,9 @@ public class Main {
         Algoritmos algoritmo = new Algoritmos();
 
         int[] orderedArray = {1,2,3,4,5,6,7,8,9,10,11,11,11,11,11,11,12};
-        System.out.println(algoritmo.binarySearch(orderedArray,11));
+        int[] unOrderedArray = {29,12,15,1,7,9,11,5,25,31};
+
+        System.out.println("Busca binária: " + algoritmo.binarySearch(orderedArray,11));
+        System.out.println("Bubble sort: " + Arrays.toString(algoritmo.bubbleSort(unOrderedArray)));
         }
     }
