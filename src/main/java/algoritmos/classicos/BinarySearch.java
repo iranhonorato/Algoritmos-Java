33b51895036e1d403aa44b1bc9a7ad6959c2d8be
iran <iranilsonhonorato88@gmail.com>
2025-11-03 15:10:16 -0300

    feat(binary_search): Implementando algoritmo de busca binária com e sem recursão

---
+++ src/main/java/algoritmos/classicos/BinarySearch.java
@@ -0,0 +1,52 @@
+package algoritmos.classicos;
+
+
+// Funciona apenas quando o array está ordenada
+public class BinarySearch {
+
+    public int function (int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int middle =  (left + right)/2;
+
+        while(array[middle] != target){
+            if (array[middle] < target){
+                left = middle + 1;
+                middle =  (left + right)/2;
+            } else if (array[middle] > target){
+                right = middle - 1;
+                middle =  (left + right)/2;
+            }
+        }
+
+        return middle;
+    }
+
+
+
+    public int recursion (int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int middle =  (left + right)/2;
+
+        while(array[middle] != target){
+            if (array[middle] < target){
+                left = middle + 1;
+                middle =  (left + right)/2;
+            } else if (array[middle] > target) {
+                right = middle - 1;
+                middle = (left + right) / 2;
+            } else {
+                if (array[middle-1] == target){
+                    int[] newArray = new int[middle-1];
+                    for (int i = 0; i < newArray.length; i++){
+                        newArray[i] = array[i];
+                        return recursion(newArray,target);
+                    }
+                }
+            }
+        }
+
+        return middle;
+    }
+}
