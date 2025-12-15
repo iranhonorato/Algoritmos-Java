952ca9ac74c45de2b58391083d1f60cc22294db6
iran <iranilsonhonorato88@gmail.com>
2025-12-15 16:06:25 -0300

    feat: Autoestudo de estrtura de dados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,15 @@ package org.example;
 import algoritmos.montanhaFrodo.Posicao;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        char letra = 'A';
+        letra++;
+        System.out.println(letra);
     }
-
-
 }
