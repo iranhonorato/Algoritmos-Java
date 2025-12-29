9ad776ec61a623c7f68f1e54e9fb363c68e1d07f
iran <iranilsonhonorato88@gmail.com>
2025-12-29 12:10:13 -0300

    feat: Implementando resumo sobre arrays

--- src/main/java/estruturaDeDados/Array.java
+++ src/main/java/estruturaDeDados/Array.java
@@ -1,22 +1,27 @@
 package estruturaDeDados;
 
 public class Array {
-
+    public static void main(String[] args) {
 //    Um array é uma estrutura de dados que
 //    armazena um conjunto de valores do mesmo
 //    tipo, organizados por índices e possuem tamanho fixo.
+
+        int[] arr = new int[5]; // Inicialização por tamanho fixo: {0, 0, 0, 0, 0}
+
+//  Índice:  0   1   2   3   4
+//  Valor:  [ ] [ ] [ ] [ ] [ ]
+
+        int[] b = {1, 2, 3, 4, 5}; // Inicialização direta
+
+        // Inicialização em linhas separadas
+        int[] c;
+        c = new int[5];
+
 //    Métodos nativos:
-//    - length()
+//    - length
 //
 //    Métodos com a classe Arrays (java.util.Arrays):
 //    Arrays.sort(numeros)
 
-//    Por exemplo:
-
-
-    int[] numeros = new int[5];
-//     Isso é algo do tipo: {0, 0, 0, 0, 0}
-//     Criamos um array com 5 posições (índices 0 a 4).
-
-
+    }
 }
